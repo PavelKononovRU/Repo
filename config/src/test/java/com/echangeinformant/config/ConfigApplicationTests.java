393ca0e3a7cd6359e4a3da4f46ed2498f31068f6
Aleksey Efremov <aleksey.i.efremov@rt.ru>
2022-12-13 10:45:26 +0300

    init

---
+++ config/src/test/java/com/echangeinformant/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echangeinformant.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
