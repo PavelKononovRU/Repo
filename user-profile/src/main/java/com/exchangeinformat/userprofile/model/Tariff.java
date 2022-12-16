45d9b00051e8442d49972023c3a907475c1fef56
Mark <mark.biktiryakov@rambler.ru>
2022-12-16 15:38:01 +0300

    create tariff model

--- user-profile/src/main/java/com/exchangeinformat/userprofile/model/Tariff.java
+++ user-profile/src/main/java/com/exchangeinformat/userprofile/model/Tariff.java
@@ -1,4 +1,44 @@
 package com.exchangeinformat.userprofile.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+
+@Table(name = "tariff")
 public class Tariff {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name="type")
+    private String type;
+
+    @Column(name = "is_active")
+    private Boolean isActive;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tariff tariff = (Tariff) o;
+        return Objects.equals(title, tariff.title) && Objects.equals(description, tariff.description) &&
+                Objects.equals(type, tariff.type) && Objects.equals(isActive, tariff.isActive);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, description, type, isActive);
+    }
 }
