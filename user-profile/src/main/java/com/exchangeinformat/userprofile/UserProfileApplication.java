2e85c68251a9656a1c231881460e0f06b0dddae4
Mark <mark.biktiryakov@rambler.ru>
2022-12-19 13:34:30 +0300

    add user-profile.properties

--- user-profile/src/main/java/com/exchangeinformat/userprofile/UserProfileApplication.java
+++ user-profile/src/main/java/com/exchangeinformat/userprofile/UserProfileApplication.java
@@ -1,12 +1,7 @@
 package com.exchangeinformat.userprofile;
 
-import com.exchangeinformat.userprofile.repository.TariffRepository;
-import com.exchangeinformat.userprofile.service.TariffService;
-import com.exchangeinformat.userprofile.service.TariffServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class UserProfileApplication {
     public static void main(String[] args) {
