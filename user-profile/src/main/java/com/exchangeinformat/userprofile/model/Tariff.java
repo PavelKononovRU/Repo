1ab004c85729569c7268e3258f7d86fe7655329a
Mark <mark.biktiryakov@rambler.ru>
2022-12-20 17:33:03 +0300

    add REST for Tariff modal

--- user-profile/src/main/java/com/exchangeinformat/userprofile/model/Tariff.java
+++ user-profile/src/main/java/com/exchangeinformat/userprofile/model/Tariff.java
@@ -1,7 +1,9 @@
 package com.exchangeinformat.userprofile.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Objects;
 
