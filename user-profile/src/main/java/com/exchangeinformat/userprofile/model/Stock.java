2bf84e7a98c9d50607e34cc128fd3e36480c51b3
Mark <mark.biktiryakov@rambler.ru>
2022-12-21 09:25:20 +0300

    refactor model Stock, Tariff, User

--- user-profile/src/main/java/com/exchangeinformat/userprofile/model/Stock.java
+++ user-profile/src/main/java/com/exchangeinformat/userprofile/model/Stock.java
@@ -1,16 +1,10 @@
 package com.exchangeinformat.userprofile.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
-import java.util.Objects;
-
-@Getter
-@Setter
+@Data
 @Entity
-@NoArgsConstructor
 @Table(name = "stocks")
 public class Stock {
 
@@ -54,23 +48,4 @@ public class Stock {
 
     @Column(name = "currency_code")
     private String currencyCode;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Stock stock = (Stock) o;
-        return Double.compare(stock.previousClosures, previousClosures) == 0 &&
-                Double.compare(stock.open, open) == 0 && Double.compare(stock.volume, volume) == 0 &&
-                Double.compare(stock.changePerYear, changePerYear) == 0 && Double.compare(stock.capital, capital) == 0 &&
-                Double.compare(stock.price, price) == 0 && Double.compare(stock.dividends, dividends) == 0 &&
-                Double.compare(stock.profit, profit) == 0 && Double.compare(stock.monthClose, monthClose) == 0 &&
-                Double.compare(stock.yearClose, yearClose) == 0 && shortName.equals(stock.shortName) &&
-                currencyCode.equals(stock.currencyCode);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(previousClosures, open, volume, changePerYear, capital, price, dividends, shortName, profit, monthClose, yearClose, currencyCode);
-    }
 }
