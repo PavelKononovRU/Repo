7181bb848cf6912d9facf3f805555f3162acc95a
Pavel Sablin <slipperlama@gmail.com>
2022-12-21 12:17:49 +0300

    add liquibase for user, job and address

--- user-profile/src/main/java/com/exchangeinformat/userprofile/model/User.java
+++ user-profile/src/main/java/com/exchangeinformat/userprofile/model/User.java
@@ -1,8 +1,7 @@
 package com.exchangeinformat.userprofile.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
@@ -14,33 +13,50 @@ import java.util.Objects;
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
 
 @Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @ToString.Include
     private Long id;
 
     @Column(name = "username")
+    @EqualsAndHashCode.Include
+    @ToString.Include
     private String username;
 
     @Column(name = "first_name")
+    @EqualsAndHashCode.Include
+    @ToString.Include
     private String firstName;
 
     @Column(name = "last_name")
+    @EqualsAndHashCode.Include
+    @ToString.Include
     private String lastName;
 
     @Column(name = "middle_name")
+    @EqualsAndHashCode.Include
+    @ToString.Include
     private String middleName;
 
     @Column(name = "birth_day")
     private Date birthDay;
 
     @Column(name = "email")
+    @EqualsAndHashCode.Include
+    @ToString.Include
     private String email;
 
     @Column(name = "phone")
+    @EqualsAndHashCode.Include
+    @ToString.Include
     private String phone;
 
     @OneToOne(cascade = CascadeType.ALL)
@@ -48,6 +64,7 @@ public class User {
     private Address address;
 
     @Column(name = "gender")
+    @EqualsAndHashCode.Include
     private String gender;
 
     @Column(name = "inn")
@@ -80,39 +97,4 @@ public class User {
     @Column(name = "token")
     private String token;
 
-    public User() {
-
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(middleName, user.middleName) && Objects.equals(birthDay, user.birthDay) && Objects.equals(email, user.email) && Objects.equals(phone, user.phone);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = 1;
-        result = 31 * result + ((id==null) ? 0 : id.hashCode());
-        result = 31 * result + ((username==null) ? 0 : username.hashCode());
-        result = 31 * result + ((firstName==null) ? 0 : firstName.hashCode());
-        result = 31 * result + ((lastName==null) ? 0 : lastName.hashCode());
-        result = 31 * result + ((middleName==null) ? 0 : middleName.hashCode());
-        result = 31 * result + ((birthDay==null) ? 0 : birthDay.hashCode());
-        result = 31 * result + ((email==null) ? 0 : email.hashCode());
-        result = 31 * result + ((phone==null) ? 0 : phone.hashCode());
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
-    }
 }
