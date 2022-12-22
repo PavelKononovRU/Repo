01a330cf4398ba14a972a719306a7f718cb8f602
Mark <mark.biktiryakov@rambler.ru>
2022-12-22 08:55:24 +0300

    add REST for model Roles

--- user-profile/src/main/java/com/exchangeinformat/userprofile/model/Roles.java
+++ user-profile/src/main/java/com/exchangeinformat/userprofile/model/Roles.java
@@ -1,11 +1,9 @@
 package com.exchangeinformat.userprofile.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 @Entity
 @Table(name = "roles")
 public class Roles {
@@ -14,19 +12,4 @@ public class Roles {
     @Column(name = "roles_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Roles roles = (Roles) o;
-        return id.equals(roles.id);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = 1;
-        return 31 * result + ((id==null) ? 0 : id.hashCode());
-    }
-
 }
