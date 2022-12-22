5dec6f18111d9cdd0e045f539b8d23e262f6c678
Excecc <e.davidenko@live.ru>
2022-12-22 14:24:12 +0300

    add String name to StockDto

--- quotes-api/src/main/java/com/exchangeinformant/dto/StockDto.java
+++ quotes-api/src/main/java/com/exchangeinformant/dto/StockDto.java
@@ -17,6 +17,18 @@ public class StockDto {
 
 
     private double currentPrice;
+
+    private String name;
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+    @JsonProperty("01. symbol")
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @JsonProperty("price")
     public double getCurrentPrice() {
         return currentPrice;
