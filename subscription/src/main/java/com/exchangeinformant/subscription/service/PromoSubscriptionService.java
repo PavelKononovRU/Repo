5f22f2272b770c0c05a0b1439541942a742e624a
SerDes010 <sdesyatoff@gmail.com>
2022-12-23 18:30:02 +0300

    add model PromoSubscription+liquibase

---
+++ subscription/src/main/java/com/exchangeinformant/subscription/service/PromoSubscriptionService.java
@@ -0,0 +1,16 @@
+package com.exchangeinformant.subscription.service;
+
+import com.exchangeinformant.subscription.model.Tariff;
+import java.util.List;
+
+public interface TariffService {
+
+    List<Tariff> getAllTariff();
+    void createTariff(Tariff tariff);
+
+    Tariff getTariff(Long id);
+
+    void updateTariff(Tariff tariff);
+
+    void deleteTariff(Long id);
+}
