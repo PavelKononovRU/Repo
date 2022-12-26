49ded9b3a23f636901dce07a1f9f4bd3bb9e1d24
MishaSkl <112400829+MishaSkl@users.noreply.github.com>
2022-12-26 17:32:46 +0300

    fix-error

--- paying-service/src/main/java/com/exchange/payingservice/Repository/CardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.exchange.payingservice.Repository;
-
-import com.exchange.payingservice.model.Card;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardRepository extends JpaRepository<Card, Long> {
-}
