7ae6785a1a5436b8c9548298a80a8aa59abd9431
Mark <bik.marc@yandex.ru>
2023-01-12 11:36:46 +0300

    add login

--- gateway-service/src/main/java/com/example/gatewayservice/config/SecurityConfig.java
+++ gateway-service/src/main/java/com/example/gatewayservice/config/SecurityConfig.java
@@ -3,24 +3,43 @@ package com.example.gatewayservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, ServerLogoutSuccessHandler handler) {
         http
                 .authorizeExchange()
-                .pathMatchers()
+                .pathMatchers("/actuator/**", "/")
                 .permitAll()
                 .and()
                 .authorizeExchange()
                 .anyExchange()
                 .authenticated()
                 .and()
-                .oauth2Login();
+                .oauth2Login()
+                .and()
+                .logout()
+                .logoutSuccessHandler(handler)
+        ;
 
         return http.build();
     }
+
+    @Bean
+    public ServerLogoutSuccessHandler keycloakLogoutSuccessHandler(ReactiveClientRegistrationRepository repository) {
+
+        OidcClientInitiatedServerLogoutSuccessHandler oidcLogoutSuccessHandler =
+                new OidcClientInitiatedServerLogoutSuccessHandler(repository);
+
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/api/user");
+
+        return oidcLogoutSuccessHandler;
+    }
+
 }
