c48ba3f64f47bc0f3572992c9b25a959bd309e6e
timur_ziya <timka_ziya@mail.ru>
2023-01-27 16:11:05 +0300

    KATA-TEAM-119_Renamed_to_Stub_payment

--- stub-payment/src/main/java/com/example/subpayment/StubPaymentApplication.java
+++ stub-payment/src/main/java/com/example/subpayment/StubPaymentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SubPaymentApplication {
+public class StubPaymentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SubPaymentApplication.class, args);
+        SpringApplication.run(StubPaymentApplication.class, args);
     }
 
 }
