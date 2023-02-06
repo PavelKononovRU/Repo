f6b6b675b06fb74300519ef983516ea3071b20df
timur_ziya <timka_ziya@mail.ru>
2023-02-06 18:07:58 +0300

    Refactored RestControllerAdvice in Paying-service. Moved RestControllerAdvice from util to Controllers.

--- paying-service/src/main/java/com/exchange/payingservice/controllers/CardRestController.java
+++ paying-service/src/main/java/com/exchange/payingservice/controllers/CardRestController.java
@@ -4,7 +4,6 @@ import com.exchange.payingservice.dto.CardDTO;
 import com.exchange.payingservice.entity.Card;
 import com.exchange.payingservice.mappers.CardMapper;
 import com.exchange.payingservice.service.CardService;
-import com.exchange.payingservice.util.RestControllerAdvice;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
