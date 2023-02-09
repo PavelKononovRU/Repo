8ec0dcbaab332bdccf6d01e6d88d6da8a03ffb24
Roman Isakov <roman.isakov.job@gmail.com>
2023-02-09 14:45:45 +0300

    KATA-TEAM-141_142_тесты+корректировка_выдачи_списка_акций_по_имени_сервиса

--- quotes-api/src/main/java/com/exchangeinformant/model/Stock.java
+++ quotes-api/src/main/java/com/exchangeinformant/model/Stock.java
@@ -1,4 +1,5 @@
 package com.exchangeinformant.model;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.*;
 import java.util.List;
@@ -8,22 +9,28 @@ import java.util.List;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Сущность акции")
 public class Stock {
     @Id
     @Column(name = "secure-code")
+    @Schema(description = "SecureCode акции (тикер)", example = "AAPL")
     private String secureCode;
 
     @Column
+    @Schema(description = "Название компании, выпустившей акцию", example = "Apple")
     private String issuer;
 
     @Column
+    @Schema(description = "Валюта", example = "USD")
     private String currency;
 
     @OneToMany(mappedBy = "secureCode")
     @EqualsAndHashCode.Exclude
+    @Schema(description = "Информация о акции")
     private List<Info> infoList;
 
     @Column
+    @Schema(description = "Подключенный сервис для получения цен акций", example = "tinkoff")
     private String source;
 
     public Stock(String secureCode, String issuer, String currency,String source) {
