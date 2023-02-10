32a52167992169c1646f095cc8fa827b05981478
Mihail Shatov <mihailshatov02@gmail.com>
2023-02-10 13:02:40 +0300

    Перефразировал message в DTO

--- user-profile/src/main/java/com/exchangeinformat/userprofile/entityDTO/UserDTO.java
+++ user-profile/src/main/java/com/exchangeinformat/userprofile/entityDTO/UserDTO.java
@@ -17,34 +17,34 @@ public class UserDTO {
 
     private Long id;
 
-    @NotBlank(message = "Поле не должно быть пустым и не содержать пробелов")
+    @NotBlank(message = "Поле не должно быть пустым и не должно содержать пробелов")
     private String username;
 
-    @NotBlank(message = "Поле не должно быть пустым и не содержать пробелов")
+    @NotBlank(message = "Поле не должно быть пустым и не должно содержать пробелов")
     private String extId;
 
-    @NotBlank(message = "Поле не должно быть пустым и не содержать пробелов")
+    @NotBlank(message = "Поле не должно быть пустым и не должно содержать пробелов")
     private String firstName;
 
-    @NotBlank(message = "Поле не должно быть пустым и не содержать пробелов")
+    @NotBlank(message = "Поле не должно быть пустым и не должно содержать пробелов")
     private String lastName;
 
-    @NotBlank(message = "Поле не должно быть пустым и не содержать пробелов")
+    @NotBlank(message = "Поле не должно быть пустым и не должно содержать пробелов")
     private String middleName;
 
     @Past(message = "Поле должно быть в прошедшем времени")
     private Date birthDay;
 
-    @Email(message = "Введите правильный формат почты и не содержать пробелов")
+    @Email(message = "Введите правильный формат почты")
     private String email;
 
     @Pattern(regexp = "^\\d{11}$") // Проверка, что телефон равен ровно 11 цифрам
     private String phone;
 
-    @NotBlank(message = "Поле не должно быть пустым")
+    @NotBlank(message = "Поле не должно быть пустым и не должно содержать пробелов")
     private String gender;
 
-    @NotBlank(message = "Поле не должно быть пустым")
+    @NotBlank(message = "Поле не должно быть пустым и не должно содержать пробелов")
     private String inn;
 
 
