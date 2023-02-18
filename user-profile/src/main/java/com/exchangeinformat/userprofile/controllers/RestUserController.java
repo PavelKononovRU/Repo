56ffb1c426ce04d1a5d60b1e9cc77382db716aca
Mihail Shatov <mihailshatov02@gmail.com>
2023-02-18 13:10:26 +0300

    Добавление swagger, добавление описания rest контроллеров для пользователя

--- user-profile/src/main/java/com/exchangeinformat/userprofile/controllers/RestUserController.java
+++ user-profile/src/main/java/com/exchangeinformat/userprofile/controllers/RestUserController.java
@@ -6,6 +6,8 @@ import com.exchangeinformat.userprofile.mappers.UserMappers;
 import com.exchangeinformat.userprofile.service.UserService;
 import com.exchangeinformat.userprofile.util.Data;
 import com.exchangeinformat.userprofile.util.ValidationResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,7 @@ import java.security.Principal;
 import java.util.List;
 import java.util.Map;
 
+@Tag(name = "Контроллер пользователя", description = "CRUD операции с пользователями")
 @RestController
 @RequestMapping("api/user")
 public class RestUserController {
@@ -29,18 +32,21 @@ public class RestUserController {
         this.userService = userService;
     }
 
-    @GetMapping("/findOne")
+    @Operation(summary = "Получение пользователя", description = "Метод принимает в параметры long ID пользователя, доступ только у админа")
+    @GetMapping("/{id}")
     @RolesAllowed({"ADMIN"})
-    public ResponseEntity<User> getUser(Long id) {
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
         return ResponseEntity.ok(userService.getUserById(id));
     }
 
+    @Operation(summary = "Получение всех пользователей", description = "Доступ только у админа")
     @GetMapping
     @RolesAllowed({"ADMIN"})
     public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
+    @Operation(summary = "Создание пользователя", description = "Метод принимает в параметры UserDTO, доступ только у админа")
     @PostMapping
     @RolesAllowed({"ADMIN"})
     public ResponseEntity<HttpStatus> createUser(@RequestBody User user) {
@@ -48,6 +54,7 @@ public class RestUserController {
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
+    @Operation(summary = "Обновление пользователя", description = "Метод принимает в параметры UserDTO (нужно передать в теле запроса), доступ только у админа")
     @PutMapping
     @RolesAllowed({"ADMIN"})
     public ResponseEntity<HttpStatus> updateUser(@RequestBody User user) {
@@ -55,13 +62,15 @@ public class RestUserController {
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
-    @DeleteMapping
+    @Operation(summary = "Удаление пользователя", description = "Метод принимает в параметры long ID, доступ только у админа")
+    @DeleteMapping("/{id}")
     @RolesAllowed({"ADMIN"})
-    public ResponseEntity<HttpStatus> deleteUser(Long id) {
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable  Long id) {
         userService.deleteUser(id);
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
+    @Operation(summary = "Домашняя страница пользователя", description = "В параметры автоматически передается Principal (самому ничего передавать не нужно), доступ у пользователя и админа")
     @GetMapping("/home")
     @RolesAllowed({"ADMIN", "USER"})
     public ResponseEntity<User>  getUserDetails(Principal principal) {
@@ -83,9 +92,10 @@ public class RestUserController {
         return ResponseEntity.ok(user);
     }
 
+    @Operation(summary = "Обновление пользователя после регистрации", description = "В параметры передается Principal(автоматически) и UserDTO (нужно передать в теле запроса), доступ у админа и пользователя")
     @PostMapping(value = "/update")
     @RolesAllowed({"USER"})
-    public ResponseEntity<ValidationResponse> getWord(Principal principal, @RequestBody @Valid UserDTO userDTO) {
+    public ResponseEntity<ValidationResponse> updatingUserFields(Principal principal, @RequestBody @Valid UserDTO userDTO) {
         Map<String, Object> cl = getExtID(principal);
         String extId = cl.get("sub").toString();
         if (!userService.isUserPresent(extId) || !extId.equals(userDTO.getExtId())) { //Проверка, что пользователь существует с таким extID и проверка равенства extID principal и переданного юзера
--- user-profile/src/main/java/com/exchangeinformat/userprofile/entityDTO/UserDTO.java
+++ user-profile/src/main/java/com/exchangeinformat/userprofile/entityDTO/UserDTO.java
@@ -1,5 +1,6 @@
 package com.exchangeinformat.userprofile.entityDTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
@@ -13,39 +14,49 @@ import java.util.Date;
 @Validated
 @Data
 @Mapper
+@Schema(name = "Пользователь", description = "Сущность пользователя")
 public class UserDTO {
 
+    @Schema(description = "Идентификатор пользователя")
     private Long id;
 
+    @Schema(description = "Никнейм пользователя")
     @NotBlank(message = "Поле не должно быть пустым и не должно содержать пробелов")
     private String username;
 
+    @Schema(description = "Идентификатор пользователя, назначенный keycloak'ом")
     @NotBlank(message = "Поле не должно быть пустым и не должно содержать пробелов")
     private String extId;
 
+    @Schema(description = "Имя пользователя")
     @NotBlank(message = "Поле не должно быть пустым и не должно содержать пробелов")
     private String firstName;
 
+    @Schema(description = "Фамилия пользователя")
     @NotBlank(message = "Поле не должно быть пустым и не должно содержать пробелов")
     private String lastName;
 
+    @Schema(description = "Отчество пользователя")
     @NotBlank(message = "Поле не должно быть пустым и не должно содержать пробелов")
     private String middleName;
 
+    @Schema(description = "Дата рождения пользователя")
     @Past(message = "Поле должно быть в прошедшем времени")
     private Date birthDay;
 
+    @Schema(description = "Почта пользователя")
     @Email(message = "Введите правильный формат почты")
     private String email;
 
+    @Schema(description = "Номер телефона пользователя", example = "89000000000")
     @Pattern(regexp = "^\\d{11}$") // Проверка, что телефон равен ровно 11 цифрам
     private String phone;
 
+    @Schema(description = "Пол пользователя")
     @NotBlank(message = "Поле не должно быть пустым и не должно содержать пробелов")
     private String gender;
 
+    @Schema(description = "Идентификационный номер налогоплательщика") //Это точно ИНН? уточнить
     @NotBlank(message = "Поле не должно быть пустым и не должно содержать пробелов")
     private String inn;
-
-
 }
