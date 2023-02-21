1d63c9082db989ea020fbf53d0198858bb0eef21
timur_ziya <timka_ziya@mail.ru>
2023-02-21 17:43:14 +0300

    Refactored listener -> Set listener destination as method, not class.

--- subscription/src/main/java/com/exchangeinformant/subscription/service/MessageFromPayment.java
+++ subscription/src/main/java/com/exchangeinformant/subscription/service/MessageFromPayment.java
@@ -1,19 +1,16 @@
 package com.exchangeinformant.subscription.service;
 
-
-
 import com.exchangeinformant.subscription.util.rabbitMQ.PaymentProof;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
-import org.springframework.stereotype.Service;
 
 import java.util.function.Consumer;
 
-
-@Service
-public class MessageFromPayment implements Consumer<Message<PaymentProof>> {
-
-    @Override
-    public void accept(Message<PaymentProof> paymentProofMessage) {
-        System.out.println("Received payment: " + paymentProofMessage.getPayload());
+@Configuration
+public class MessageFromPayment {
+    @Bean
+    public Consumer<Message<PaymentProof>> getPaymentProof() {
+        return (paymentProof) -> System.out.println("Hi from listener" + paymentProof.getPayload());
     }
 }
