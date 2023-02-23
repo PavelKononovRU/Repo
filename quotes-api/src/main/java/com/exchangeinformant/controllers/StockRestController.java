4645333a4e4070e52eccc4b4cabcc66b7c614ad8
Pavel Sablin <slipperlama@gmail.com>
2023-02-23 22:59:02 +0300

    Add AOP logic to all methods in Controller

--- quotes-api/src/main/java/com/exchangeinformant/controllers/StockRestController.java
+++ quotes-api/src/main/java/com/exchangeinformant/controllers/StockRestController.java
@@ -1,5 +1,6 @@
 package com.exchangeinformant.controllers;
 
+import com.exchangeinformant.aop.UserInfoInsertion;
 import com.exchangeinformant.model.Stock;
 import com.exchangeinformant.services.StockDbService;
 import com.exchangeinformant.services.StockService;
@@ -7,6 +8,7 @@ import com.exchangeinformant.services.UserInfoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -21,71 +23,66 @@ import java.util.List;
 
 @RestController
 @Tag(name = "Контроллер Quotes", description = "Позволяет получить список всех акций, а также цены на акции")
+@Slf4j
 public class StockRestController {
-
     private final StockDbService stockDbService;
     private final StockService stockService;
-    private final UserInfoService userInfoService;
 
-    public StockRestController(StockDbService stockDbService, StockService stockService,
-                               UserInfoService userInfoService) {
+    public StockRestController(StockDbService stockDbService, StockService stockService) {
         this.stockDbService = stockDbService;
         this.stockService = stockService;
-        this.userInfoService = userInfoService;
     }
 
     @Operation(summary = "Получение акции из БД по её SecureCode (тикеру)")
     @GetMapping("/stock")
-    public Stock getStock(@RequestParam("name") @Parameter(description = "SecureCode акции (тикер)") String secureCode, Principal principal) {
-
-        userInfoService.save(principal);
+    @UserInfoInsertion
+    public Stock getStock(@RequestParam("name") @Parameter(description = "SecureCode акции (тикер)") String secureCode) {
         return stockDbService.getStock(secureCode);
     }
 
     @Operation(summary = "Получение акции из БД по её SecureCode (тикеру) с ценами за текущий день")
     @GetMapping("/stock/query")
+    @UserInfoInsertion
     public Stock getStockWithParameters(@RequestParam(name = "dateFrom", required = false, defaultValue = "#{T(java.time.LocalDateTime).now().toLocalDate().atStartOfDay()}") LocalDateTime dateFrom,
                                                     @RequestParam(name = "dateTo", required = false, defaultValue = "#{T(java.time.LocalDateTime).now()}") LocalDateTime dateTo,
-                                                    @RequestParam("stock") @Parameter(description = "SecureCode акции (тикер)") String secureCode, Principal principal) {
-        userInfoService.save(principal);
+                                                    @RequestParam("stock") @Parameter(description = "SecureCode акции (тикер)") String secureCode) {
         return stockDbService.getStockByDate(secureCode, dateFrom, dateTo);
     }
 
     @Operation(summary = "Получение всех акций из БД с ценами за текущий день")
     @GetMapping("/allWithDates")
+    @UserInfoInsertion
     public List<Stock> getAllStocksByDates(@RequestParam(name = "dateFrom", required = false, defaultValue = "#{T(java.time.LocalDateTime).now().toLocalDate().atStartOfDay()}") LocalDateTime dateFrom,
-                                           @RequestParam(name = "dateTo", required = false, defaultValue = "#{T(java.time.LocalDateTime).now()}") LocalDateTime dateTo, Principal principal)
+                                           @RequestParam(name = "dateTo", required = false, defaultValue = "#{T(java.time.LocalDateTime).now()}") LocalDateTime dateTo)
     {
-        userInfoService.save(principal);
         return stockDbService.getAllStocksByDate(dateFrom, dateTo);
     }
 
     @Operation(summary = "Получение списка всех акций из БД")
     @GetMapping("/all")
-    public List<Stock> getAllStocks(Principal principal)
-    {
-        userInfoService.save(principal);
+    @UserInfoInsertion
+    public List<Stock> getAllStocks(){
         return stockDbService.getAllStocks();
     }
 
     @Operation(summary = "Получение списка акций из БД по их SecureCode (тикерам)")
     @PostMapping("/availableStocks")
-    public List<Stock> getAllAvailableStocks(@RequestBody List<String> securityCodes, Principal principal) {
-        userInfoService.save(principal);
+    @UserInfoInsertion
+    public List<Stock> getAllAvailableStocks(@RequestBody List<String> securityCodes) {
         return stockDbService.getAllAvailableStocksByCodes(securityCodes);
     }
     
     @Operation(summary = "Получение акции по её SecureCode (тикеру) напрямую с подключенного сервиса (BCS или Tinkoff)")
     @GetMapping("/directStock")
-    public Stock getStockDirectlyByBcs(@RequestParam("name") @Parameter(description = "SecureCode акции (тикер)") String secureCode, Principal principal) {
-        userInfoService.save(principal);
+    @UserInfoInsertion
+    public Stock getStockDirectlyByBcs(@RequestParam("name") @Parameter(description = "SecureCode акции (тикер)") String secureCode) {
         return stockService.getStockDirectly(secureCode);
     }
 
     @Operation(summary = "Получение списка акций по их SecureCode (тикерам) напрямую с подключенного сервиса (BCS или Tinkoff)")
     @PostMapping("/directStock")
-    public List<Stock> getStocksDirectlyByBcs(@RequestBody List<String> secureCodes, Principal principal) {
-        userInfoService.save(principal);
+    @UserInfoInsertion
+    public List<Stock> getStocksDirectlyByBcs(@RequestBody List<String> secureCodes) {
         return stockService.getStocksDirectly(secureCodes);
     }
 }
